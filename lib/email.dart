5ff009ba4a8426ce71307d7893dca6834b862a78
Zima <94643116+zimajones@users.noreply.github.com>
2022-10-23 21:57:53 +0200

    added buttons and email page

--- lib/email.dart
+++ lib/email.dart
@@ -23,38 +23,75 @@ class _EmailPageState extends State<EmailPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Email Me'),
+      appBar: AppBar(
+        title: const Text('Email Me'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(25, 40, 25, 0),
+        child: Form(
+            child: Column(
+          children: [
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.account_circle),
+                hintText: 'Name',
+                labelText: 'Name',
+              ),
             ),
-            body: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 40, 25, 0),
-              child: Form(
-                  child: Column(
-                children: [
-                  TextFormField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.account_circle),
-                      hintText: 'Name',
-                      labelText: 'Name',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  TextFormField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.subject_rounded),
-                      hintText: 'Subject',
-                      labelText: 'Subject',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  )
-                ],
-              )),
-            )));
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              controller: subjectController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.subject_rounded),
+                hintText: 'Subject',
+                labelText: 'Subject',
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.email),
+                hintText: 'Email',
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              controller: messageController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.message),
+                hintText: 'Message',
+                labelText: 'Message',
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Send'),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Back')),
+              ],
+            )
+          ],
+        )),
+      ),
+    ));
   }
 }
